ee0fdfa005425657d964c44860ac357ed3793be7
bhaddow <bhaddow@bdbe251c-f29d-11dd-9438-511482e5b71c>
2009-03-20 18:32:09 +0000

    Creation of tool chains

--- translation-server/tbroker/src/org/statmt/tbroker/ToolChain.java
+++ translation-server/tbroker/src/org/statmt/tbroker/ToolChain.java
@@ -13,11 +13,20 @@
 package org.statmt.tbroker;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+
+import org.apache.log4j.Logger;
 
 public class ToolChain extends TranslationTool {
     
+    private static final Logger _logger = Logger.getLogger(ToolChain.class);
+    
     private List<TranslationTool> _tools = new ArrayList<TranslationTool>();
+    
+    public ToolChain(String name) {
+        super(name);
+    }
     
     public void addTool(TranslationTool tool) {
         _tools.add(tool);
@@ -25,8 +34,11 @@ public class ToolChain extends TranslationTool {
 
     @Override
     public String[] transform(String[] input) {
+        _logger.debug("Toolchain " + getName() + " processing request");
         for (TranslationTool tool: _tools) {
+            _logger.debug(tool.getName() + "> " + Arrays.toString(input));
             input = tool.transform(input);
+            _logger.debug(tool.getName() +"< " + Arrays.toString(input));
         }
         return input;
     }
