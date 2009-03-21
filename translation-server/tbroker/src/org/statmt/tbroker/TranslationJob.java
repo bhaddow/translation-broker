86978400bb65be18be66df37dbf9a27c645d64e8
bhaddow <bhaddow@bdbe251c-f29d-11dd-9438-511482e5b71c>
2009-03-21 21:11:08 +0000

    translation job listing of chains

---
+++ translation-server/tbroker/src/org/statmt/tbroker/TranslationJob.java
@@ -0,0 +1,53 @@
+package org.statmt.tbroker;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.xmlrpc.XmlRpcException;
+
+/**
+ * Wraps a translation job as it proceeds through the tool chain.
+ * @author bhaddow
+ *
+ */
+public class TranslationJob {
+	
+	public static final String FIELD_TEXT = "text";
+	public static final String FIELD_SYSID = "systemid";
+	
+	private String _text;
+	private String _systemId;
+
+	/**
+	 * Does the demarshalling
+	 * @param params
+	 */
+	public TranslationJob(Map params) throws XmlRpcException{
+		_text = (String)params.get(FIELD_TEXT);
+		if (_text == null) {
+			throw new XmlRpcException("Missing text");
+		}
+		_systemId = (String)params.get(FIELD_SYSID);
+		if (_systemId == null) {
+			throw new XmlRpcException("Missing system id");
+		}
+	}
+	
+	public String getSystemId() {
+		return _systemId;
+	}
+	
+	public String getText() {
+		return _text;
+	}
+	
+	public void setText(String text) {
+		_text = text;
+	}
+	
+	public Map getResult() {
+		Map result = new HashMap();
+		result.put(FIELD_TEXT, _text);
+		return result;
+	}
+}
