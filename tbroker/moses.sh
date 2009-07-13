9e2608247c69a3e1e424835f53b795690527ebba
bhaddow <bhaddow@bdbe251c-f29d-11dd-9438-511482e5b71c>
2009-07-13 14:40:41 +0000

    config changes for moses servers

---
+++ tbroker/moses.sh
@@ -0,0 +1,50 @@
+#!/bin/sh
+
+#
+# Starts/stops the moses servers
+#
+
+pidfile=/disk4/translation-server/pids
+port=8080
+configs="/disk4/translation-server/models/fr-en-news-commentary/moses.ini"
+mosesserver=/disk3/bhaddow/moses-server/server/mosesserver
+logdir=/disk4/translation-server/logs
+
+
+start() {
+    echo "Starting moses servers"
+    if [ -f $pidfile ]; then
+        echo >&2 "Error: moses already running"
+        exit 1
+    fi
+    for config in $configs; do
+        nohup $mosesserver -f $config --server-port $port >& $logdir/mosesserver.$port.log &
+        echo "$!" >> $pidfile
+        port=`expr $port + 1`
+    done
+
+}
+
+stop() {
+    echo "Stopping moses servers"
+    if [ ! -f $pidfile ]; then
+        echo >&2 "Error: moses not running"
+        exit 1
+    fi
+    for pid in `cat $pidfile`; do
+        kill $pid
+    done
+    rm $pidfile
+}
+
+case "$1" in
+    start)
+        start
+        ;;
+    stop)
+        stop
+        ;;
+    *)
+    echo $"Usage: $0 {start|stop}"
+    exit 1
+esac
