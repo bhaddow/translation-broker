edfa7ff12d2d34f03844ec6b03b587bf68f7979f
bhaddow <bhaddow@bdbe251c-f29d-11dd-9438-511482e5b71c>
2009-07-29 11:22:04 +0000

    ec config

---
+++ tbroker/tbroker.ec.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+here=`dirname $0`
+classpath=`ls $here/lib/*`
+classpath=`echo $classpath | sed -e 's/ /:/g'`
+classpath=$here/config:$here/bin:$classpath
+
+java -classpath $classpath -Dlog4j.configuration=log4j-ec.properties  org.statmt.tbroker.Main config/thor-ec.xml
