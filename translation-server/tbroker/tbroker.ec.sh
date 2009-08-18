7aca8399ee25b8701a4beb7bf3a09ab79645de23
bhaddow <bhaddow@bdbe251c-f29d-11dd-9438-511482e5b71c>
2009-08-18 15:40:15 +0000

    location of config

--- translation-server/tbroker/tbroker.ec.sh
+++ translation-server/tbroker/tbroker.ec.sh
@@ -6,4 +6,4 @@ classpath=`echo $classpath | sed -e 's/ /:/g'`
 classpath=$here/config:$here/bin:$classpath
 logdir=$here/../logs.ec
 
-nohup java -classpath $classpath -Dlog4j.configuration=log4j.ec.properties  org.statmt.tbroker.Main config/thor-ec.xml >> $logdir/tbroker.out 2>> $logdir/tbroker.err &
+nohup java -classpath $classpath -Dlog4j.configuration=log4j.ec.properties  org.statmt.tbroker.Main $here/config/thor-ec.xml >> $logdir/tbroker.out 2>> $logdir/tbroker.err &
--- translation-server/tbroker/tbroker.prod.sh
+++ translation-server/tbroker/tbroker.prod.sh
@@ -6,4 +6,4 @@ classpath=`echo $classpath | sed -e 's/ /:/g'`
 classpath=$here/config:$here/bin:$classpath
 logdir=$here/../logs
 
-nohup java -classpath $classpath -Dlog4j.configuration=log4j.prod.properties  org.statmt.tbroker.Main config/thor-prod.xml >> $logdir/tbroker.out 2>> $logdir/tbroker.err &
+nohup java -classpath $classpath -Dlog4j.configuration=log4j.prod.properties  org.statmt.tbroker.Main $here/config/thor-prod.xml >> $logdir/tbroker.out 2>> $logdir/tbroker.err &
