75ef3c3bfc716056c921343af199013bb22d4656
abmayne <abmayne@bdbe251c-f29d-11dd-9438-511482e5b71c>
2014-03-03 10:27:57 +0000

    ctm sentence splitter is a pipedSentenceSplitting tool

--- translation-server/tbroker/tbroker.eub.sh
+++ translation-server/tbroker/tbroker.eub.sh
@@ -6,4 +6,5 @@ classpath=`echo $classpath | sed -e 's/ /:/g'`
 classpath=$here/config:$here/bin:$classpath
 logdir=$here/../logs.eub
 
-nohup java -classpath $classpath -Dlog4j.configuration=log4j.eub.properties  org.statmt.tbroker.Main $here/config/thor-eub.xml >> $logdir/tbroker.out 2>> $logdir/tbroker.err &
+#nohup java -classpath $classpath -Dlog4j.configuration=log4j.eub.properties  org.statmt.tbroker.Main $here/config/thor-eub.xml >> $logdir/tbroker.out 2>> $logdir/tbroker.err &
+nohup java -classpath $classpath -Dlog4j.configuration=log4j.eub.properties  org.statmt.tbroker.Main $here/config/thor-eub-test.xml >> $logdir/tbroker.out 2>> $logdir/tbroker.err &
